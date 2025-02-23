19d26e884b054daa3be500541146bf955f96255a
Peter Peng <3457393079@qq.com>
2025-02-23 20:20:26 +0800

    编写渲染中

---
+++ HelixEngine/include/HelixEngine/Render/SwapChain.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <nvrhi/nvrhi.h>
+#include <nvrhi/d3d12.h>
+
+namespace helix::render
+{
+	class SwapChain final
+	{
+	public:
+	private:
+		nvrhi::TextureHandle textureBuffers;
+	};
+}
