101ab450a31d449baf0d64e0e533ed59922c5fdf
Peter Peng <3457393079@qq.com>
2025-04-30 21:36:39 +0800

    准备自行编写侵入式链表

--- HelixEngine/include/HelixEngine/Node/Node.hpp
+++ HelixEngine/include/HelixEngine/Node/Node.hpp
@@ -1,31 +1,43 @@
 #pragma once
-#include <HelixEngine/Util/Ref.hpp>
-#include <HelixEngine/Util/Time.hpp>
+#include <ranges>
 #include <HelixEngine/Math/Transform.hpp>
 #include <HelixEngine/Render/Renderer.hpp>
+#include <HelixEngine/Util/Ref.hpp>
+#include <HelixEngine/Util/Time.hpp>
 #include <boost/intrusive/list.hpp>
-#include <ranges>
+#include <HelixEngine/Util/Logger.hpp>
 
 namespace helix
 {
 	class Node2D : public Object
 	{
 	protected:
-		virtual void update(Duration deltaTime) = 0;
-		virtual void render(Renderer* renderer) = 0;
+		virtual void update(Duration deltaTime);
+		virtual void render(Renderer* renderer);
 	private:
 		Transform2D transform;
 
 		struct IntrusiveHook : boost::intrusive::list_base_hook<>
 		{
 			Ref<Node2D> node;
+			~IntrusiveHook() { node.reset(); }
 		} intrusiveHook;
 
 		using IntrusiveList = boost::intrusive::list<IntrusiveHook>;
 		IntrusiveList children;
 	public:
+		using ChildrenView = std::ranges::transform_view<
+			std::ranges::ref_view<const boost::intrusive::list<IntrusiveHook>>, std::function<Ref<Node2D>(
+					const IntrusiveHook&)>>;
 		//等待测试
-		void addNode(const Ref<Node2D>& child);
-		void removeNode(const Ref<Node2D>& child);
+		void addChild(const Ref<Node2D>& child);
+		void removeChild(const Ref<Node2D>& child);
+		[[nodiscard]] ChildrenView getAllChildren() const;
+
+		~Node2D() override
+		{
+			children.clear();
+			Logger::info(u8"Node destroyed:", getName());
+		}
 	};
 }
--- HelixEngine/src/Node/Node.cpp
+++ HelixEngine/src/Node/Node.cpp
@@ -1,18 +1,31 @@
 #include <HelixEngine/Node/Node.hpp>
 #include <HelixEngine/Core/Window.hpp>
 
-void helix::Node2D::addNode(const Ref<Node2D>& child)
+void helix::Node2D::update(Duration deltaTime)
+{
+}
+
+void helix::Node2D::render(Renderer* renderer)
+{
+}
+
+void helix::Node2D::addChild(const Ref<Node2D>& child)
 {
 	child->intrusiveHook.node = child;
 	children.push_back(child->intrusiveHook);
-	// auto a = children | std::views::transform([](const IntrusiveHook& hook) { return hook.node; });
-	// for (const auto& node2d: a)
-	// {
-	//
-	// }
 }
 
-void helix::Node2D::removeNode(const Ref<Node2D>& child)
+void helix::Node2D::removeChild(const Ref<Node2D>& child)
 {
-	children.remove(child->intrusiveHook);
+	child->intrusiveHook.node = nullptr;
+	children.remove_if([&child](const IntrusiveHook& hook) { return child->intrusiveHook.node == hook.node; });
+}
+
+helix::Node2D::ChildrenView helix::Node2D::getAllChildren() const
+{
+	return children | std::views::transform(
+			       std::function([](const IntrusiveHook& hook)
+			       {
+				       return hook.node;
+			       }));
 }
