b92fd5829025ad9a6af979410d4dd0ee0e7ea2e6
Peter Peng <3457393079@qq.com>
2025-08-05 17:07:19 +0800

    添加Vector4

--- HelixEngine/include/HelixEngine/Math/Vector.hpp
+++ HelixEngine/include/HelixEngine/Math/Vector.hpp
@@ -12,11 +12,15 @@ namespace helix
 	class Vector2T;
 	template<Arithmetic T>
 	class Vector3T;
+	template<Arithmetic T>
+	class Vector4T;
 
 	template<Enum T>
 	class Vector2E;
 	template<Enum T>
 	class Vector3E;
+	template<Enum T>
+	class Vector4E;
 }
 
 namespace ktm
@@ -54,10 +58,160 @@ namespace ktm
 		using base_type = T;
 		static constexpr size_t len = 3;
 	};
+
+	template<typename T>
+	struct is_vector<helix::Vector4T<T>> : std::true_type
+	{
+	};
+
+	template<typename T>
+	struct is_floating_point_base<helix::Vector4T<T>> : std::true_type
+	{
+	};
+
+	template<typename T>
+	struct math_traits<helix::Vector4T<T>>
+	{
+		using base_type = T;
+		static constexpr size_t len = 4;
+	};
 }
 
 namespace helix
 {
+	template<Arithmetic T>
+	class Vector4T
+	{
+	public:
+		using KtmVec = ktm::vec<4, T>;
+		T x{};
+		T y{};
+		T z{};
+		T w{};
+
+		Vector4T() = default;
+
+		explicit Vector4T(KtmVec ktmVec) :
+			x(ktmVec.x), y(ktmVec.y), z(ktmVec.z), w(ktmVec.w)
+		{
+		}
+
+		Vector4T(T x, T y, T z, T w):
+			x(x), y(y), z(z), w(w)
+		{
+		}
+
+		auto getLength() const
+		{
+			return ktm::length(ktmVecSelf());
+		}
+
+		Vector4T& normalize()
+		{
+			ktmVecSelf() = ktm::normalize(ktmVecSelf());
+			return *this;
+		}
+
+		bool operator==(const Vector4T& vec4) const = default;
+
+		Vector4T operator*(T value) const
+		{
+			return Vector4T(x * value, y * value, z * value, w * value);
+		}
+
+		static Vector4T normalize(const Vector4T& vector3)
+		{
+			return Vector4T{ktm::normalize(vector3.ktmVecSelf())};
+		}
+
+		explicit operator KtmVec() const
+		{
+			return KtmVec(x, y, z);
+		}
+
+		template<typename OtherT>
+		explicit operator Vector4T<OtherT>() const
+		{
+			return Vector4T<OtherT>(static_cast<OtherT>(x), static_cast<OtherT>(y), static_cast<OtherT>(z),
+			                        static_cast<OtherT>(w));
+		}
+
+		Vector2T<T>& vector2()
+		{
+			return *reinterpret_cast<Vector2T<T>*>(this);
+		}
+
+		const Vector2T<T>& vector2() const
+		{
+			return *reinterpret_cast<const Vector2T<T>*>(this);
+		}
+
+		Vector3T<T>& vector3()
+		{
+			return *reinterpret_cast<Vector3T<T>*>(this);
+		}
+
+		const Vector3T<T>& vector3() const
+		{
+			return *reinterpret_cast<const Vector3T<T>*>(this);
+		}
+	private:
+		auto& ktmVecSelf()
+		{
+			return *reinterpret_cast<KtmVec*>(this);
+		}
+
+		const auto& ktmVecSelf() const
+		{
+			return *reinterpret_cast<const KtmVec*>(this);
+		}
+	};
+
+	template<Enum T>
+	class Vector4E
+	{
+	public:
+		T x{};
+		T y{};
+		T z{};
+		T w{};
+		Vector4E() = default;
+
+		Vector4E(T x, T y, T z, T w):
+			x(x), y(y), z(z), w(w)
+		{
+		}
+
+		template<typename OtherT>
+		explicit operator Vector4T<OtherT>() const
+		{
+			return Vector4T<OtherT>(static_cast<OtherT>(x), static_cast<OtherT>(y), static_cast<OtherT>(z),
+			                        static_cast<OtherT>(w));
+		}
+
+		Vector2E<T>& vector2()
+		{
+			return *reinterpret_cast<Vector2E<T>*>(this);
+		}
+
+		const Vector2E<T>& vector2() const
+		{
+			return *reinterpret_cast<const Vector2E<T>*>(this);
+		}
+
+		Vector3E<T>& vector3()
+		{
+			return *reinterpret_cast<Vector3E<T>*>(this);
+		}
+
+		const Vector3E<T>& vector3() const
+		{
+			return *reinterpret_cast<const Vector3E<T>*>(this);
+		}
+
+		bool operator==(const Vector4E& vec4) const = default;
+	};
+
 	template<Arithmetic T>
 	class Vector3T
 	{
@@ -122,6 +276,11 @@ namespace helix
 		{
 			return *reinterpret_cast<const Vector2T<T>*>(this);
 		}
+
+		Vector4T<T> vector4(T w = {}) const
+		{
+			return Vector4T<T>(x, y, z, w);
+		}
 	private:
 		auto& ktmVecSelf()
 		{
@@ -162,6 +321,11 @@ namespace helix
 		const Vector2E<T>& vector2() const
 		{
 			return *reinterpret_cast<const Vector2E<T>*>(this);
+		}
+
+		Vector4T<T> vector4(T w = T{}) const
+		{
+			return Vector4T<T>(x, y, z, w);
 		}
 
 		bool operator==(const Vector3E& vec3) const = default;
@@ -225,6 +389,11 @@ namespace helix
 		{
 			return Vector3T<T>(x, y, z);
 		}
+
+		Vector4T<T> vector4(T z = T{}, T w = T{}) const
+		{
+			return Vector4T<T>(x, y, z, w);
+		}
 	private:
 		auto& ktmVecSelf()
 		{
@@ -262,6 +431,11 @@ namespace helix
 			return Vector3E<T>(x, y, z);
 		}
 
+		Vector4E<T> vector4(T z = T{}, T w = T{}) const
+		{
+			return Vector4E<T>(x, y, z, w);
+		}
+
 		bool operator==(const Vector2E& vec2) const = default;
 	};
 
@@ -286,6 +460,17 @@ namespace helix
 	using Vector3UI32 = Vector3T<uint32_t>;
 	using Vector3I64 = Vector3T<int64_t>;
 	using Vector3UI64 = Vector3T<uint64_t>;
+
+	using Vector4F = Vector4T<float>;
+	using Vector4Double = Vector4T<double>;
+	using Vector4I8 = Vector4T<int8_t>;
+	using Vector4UI8 = Vector4T<uint8_t>;
+	using Vector4I16 = Vector4T<int16_t>;
+	using Vector4UI16 = Vector4T<uint16_t>;
+	using Vector4I32 = Vector4T<int32_t>;
+	using Vector4UI32 = Vector4T<uint32_t>;
+	using Vector4I64 = Vector4T<int64_t>;
+	using Vector4UI64 = Vector4T<uint64_t>;
 
 	template<typename T>
 	struct IsVector2
@@ -327,14 +512,35 @@ namespace helix
 	};
 
 	template<typename T>
+	struct IsVector4
+	{
+		static constexpr bool value = false;
+	};
+
+	template<typename T>
+	struct IsVector4<Vector4T<T>>
+	{
+		static constexpr bool value = true;
+	};
+
+	template<typename T>
+	struct IsVector4<Vector4E<T>>
+	{
+		static constexpr bool value = true;
+	};
+
+	template<typename T>
+	concept Vector4Type = IsVector4<T>::value;
+
+	template<typename T>
 	struct IsVector
 	{
-		static constexpr bool value = IsVector2<T>::value || IsVector3<T>::value;
+		static constexpr bool value = IsVector2<T>::value || IsVector3<T>::value || IsVector4<T>::value;
 	};
 
 	template<typename T>
 	concept Vector3Type = IsVector3<T>::value;
 
 	template<typename T>
-	concept VectorType = Vector2Type<T> || Vector3Type<T>;
+	concept VectorType = IsVector<T>::value;
 }
