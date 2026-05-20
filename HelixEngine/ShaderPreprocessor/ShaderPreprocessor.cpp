f2a00eb0e36e683e3da95d988f97969d150a81cb
FriesPeter <3457393079@qq.com>
2026-05-20 13:22:17 +0800

    完成命令行参数解析

--- HelixEngine/ShaderPreprocessor/ShaderPreprocessor.cpp
+++ HelixEngine/ShaderPreprocessor/ShaderPreprocessor.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <variant>
 #include <HelixEngine/Util/Logger.hpp>
+#include <glslang/Public/ShaderLang.h>
 using namespace helix;
 
 class CommandLineProcessor
@@ -10,7 +12,7 @@ public:
 	{
 		std::vector<std::string> argNames;
 		size_t argSize = 0;
-		std::variant<std::string,std::vector<std::string>> argContent;
+		std::vector<std::string> argContent;
 	};
 
 	CommandLineProcessor(int argc,char** argv) : argc(argc), argv(argv)
@@ -35,10 +37,38 @@ public:
 		//参数解析
 		for (int i = 1; i < argc;)
 		{
+			bool isParsed = false;
 			for (auto& arg : args)
 			{
+				bool found = false;
+				for (const auto & arg_name: arg.argNames)
+				{
+					if (arg_name == argv[i])
+					{
+						found = true;
+						break;
+					}
+				}
+				if (!found) continue;
 				++i;
-				//上次的进度
+				if (argc - i < arg.argSize)
+				{
+					Logger::error(u8"arguments input error");
+					return false;
+				}
+				arg.argContent.resize(arg.argSize);
+				for (size_t j = 0; j < arg.argSize; ++j)
+				{
+					arg.argContent[j] = argv[i];
+					++i;
+				}
+				isParsed = true;
+				break;
+			}
+			if (!isParsed)
+			{
+				Logger::error(u8"Unparsed argument: ", std::u8string(reinterpret_cast<char8_t*>(argv[i])));
+				return false;
 			}
 		}
 		return true;
@@ -59,9 +89,14 @@ int main(int argc, char* argv[])
 	CommandLineProcessor clp(argc,argv);
 	std::vector<CommandLineProcessor::Arg> args = {
 		{{"-o"}, 1},
+		{{"-o"}, 1},
 	};
 
-	clp.process(args);
+	if (!clp.process(args))
+	{
+		Logger::error(u8"Command line parse error");
+		return 1;
+	}
 
 	return 0;
 }
